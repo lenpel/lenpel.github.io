de068de67ec3372917f96812186042ff3283fcf8
lenpel <26303224+lenpel@users.noreply.github.com>
2019-06-18 10:11:43 +0200

    import from codepen

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Portfolio LenPel LIGHT
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/lenpel/pen/joYXmX](https://codepen.io/lenpel/pen/joYXmX).
+
+ 
---
+++ src/script.js
@@ -0,0 +1,48 @@
+// close responsive menu after click event
+$('.navbar-collapse ul li a').click(function(){
+  $('.navbar-toggle:visible').click();
+});
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+// fly in animation
+$(function () {
+  setTimeout(function () {
+    $(".fly-in-text").removeClass('hidden');
+  }, 800);
+})();
+
+// adding smooth scrolling - as it is not supported by Safari
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+// End of adding smooth scrolling - as it is not supported by Safari
+
