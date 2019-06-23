dd136b1b2f9ae636c027a8b656e22c9d17a7ff92
lenpel <26303224+lenpel@users.noreply.github.com>
2019-06-24 00:11:27 +0200

    mostly css changes

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ $(function () {
   });
 });
 
+// contact form -> send message and display thank you alert
+var message = "";
+
+$("#sendMessage").on("click", function() {
+    message = $("#contactform").serialize();
+    $.ajax({
+        url: "//formspree.io/dynamicrealities@gmail.com", 
+        method: "POST",
+        data: {message: message},
+        dataType: "json"
+    });
+    alert('Thanks for the email, we\'ll be in touch promptly.');
+    return false;
+});
+
 // adding smooth scrolling - as it is not supported by Safari
 
 $(document).ready(function(){
